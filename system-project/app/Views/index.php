8ee7356e071093810a132fa2ff65a72a74498383
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-06-17 11:23:35 +0800

    bootstrap added

--- system-project/app/Views/index.php
+++ system-project/app/Views/index.php
@@ -7,7 +7,22 @@
     </main>
     <script>
         $(document).ready(function() {
-           
+           const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+Toast.fire({
+  icon: 'success',
+  title: 'Signed in successfully'
+})
         })
     </script>
     <!-- footer auto generated in this section -->
--- system-project/app/Views/templates/Headers.php
+++ system-project/app/Views/templates/Headers.php
@@ -5,5 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="./node_modules/sweetalert2/dist/sweetalert2.all.min.js"></script>
+    
     <script src="./node_modules/jquery/dist/jquery.min.js"></script>
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <script src="./node_modules/bootstrap/dist/js/bootstrap.bundle.js"></script>
     
